f4b23b6915b2d826450cec85288173988c889d0d
Maximo Machado <maximomachado246@gmail.com>
2026-05-16 17:40:25 -0700

    Add apt/brewfile and improve config.fish readability

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,41 +1,43 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
-if type -q lsd
-    starship init fish | source
-end
 
-alias zel="zellij"
+    # If starship is file/binary/function
+    if type -q starship
+        starship init fish | source
+    end
 
-if type -q lsd
-    alias ls="lsd -l"
-end
+    if type -q zellij;  and alias zel="zellij"
+    if type -q lsd;     and alias ls="lsd -l"
+    if type -q duf;     and alias df="duf"
+    if type -q fdfind;  and alias fd="fdfind"
+    if type -q rg;      and alias rg="rg -S"
 
-if type -q fdfind
-    alias fd="fdfind"
-end
+    if type -q nvim
+        set --global --export EDITOR nvim
+        alias vim="nvim"
+    else
+        set -gx EDITOR vim
+    end
 
-if type -q nvim
-    set -gx EDITOR nvim
-    alias vim="nvim"
-else
-    set -gx EDITOR vim
-end
+    if test -f $HOME/.ssh/golden-finch 
+        ssh-add $HOME/.ssh/golden-finch
+    end
 
-ssh-add $HOME/.ssh/golden-finch
+    # opencode
+    fish_add_path $HOME/.opencode/bin
+    fish_add_path /usr/local/cuda/bin
+    fish_add_path $HOME/build/llama.cpp/build/bin
 
-# opencode
-fish_add_path $HOME/.opencode/bin
-fish_add_path /usr/local/cuda/bin
-fish_add_path $HOME/build/llama.cpp/build/bin
+    set -gx LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
 
-set -gx LD_LIBRARY_PATH /usr/local/cuda/lib64 $LD_LIBRARY_PATH
-
-# If path to brew bin exists, then source needed ENV vars
-if test -f /home/linuxbrew/.linuxbrew/bin/brew
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
-end
+    # If path to brew bin exists, then source needed ENV vars
+    if test -f /home/linuxbrew/.linuxbrew/bin/brew
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+    end
 
 end # is-interactive
 
-set -gx PATH "/home/maximo/.pixi/bin" $PATH
+# Test if directory
+if test -d $HOME/.pixi/bin
+    set --global --export PATH "$HOME/.pixi/bin" $PATH
+end
 
